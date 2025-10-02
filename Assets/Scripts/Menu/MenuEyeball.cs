0db965dc71306623d22d871fdb403feb1643a3e1
Generalisk <80116907+Generalisk@users.noreply.github.com>
2025-10-02 11:09:31 +0100

    Add menu eyeball look at offset

--- Assets/Scripts/Menu/MenuEyeball.cs
+++ Assets/Scripts/Menu/MenuEyeball.cs
@@ -3,11 +3,18 @@ using UnityEngine.InputSystem;
 
 public class MenuEyeball : MonoBehaviour
 {
+    public Vector3 offset;
+    private Vector3 forward;
+
+    void Start() => forward = transform.localEulerAngles;
+
     void Update()
     {
         var rawCursorPos = Mouse.current.position;
         Vector3 cursorPos = rawCursorPos.ReadValue();
         cursorPos.z = 0.5f;
         var cursorPosWorld = Camera.main.ScreenToWorldPoint(cursorPos);
+        transform.LookAt(cursorPosWorld);
+        transform.Rotate(forward - offset);
     }
 }
