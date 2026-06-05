6af6c0803028befb1fe74d1acca7887b261c8b06
Mate93934 <mateodjm@gmail.com>
2026-06-05 03:27:08 -0300

    refactor(camera): make camera follow player instead of rooms

--- Assets/Scripts/Managers/CameraController.cs
+++ Assets/Scripts/Managers/CameraController.cs
@@ -1,63 +1,30 @@
-using System;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public static CameraController Instance { get; private set; }
-
+    [SerializeField] private Camera cam;
     [SerializeField] private Transform player;
     [SerializeField] private float smoothTime = 0.3f;
 
-    private Vector3 velocity;
-    private Vector3 targetPos;
-    private bool isMoving = false;
-    private Room targetRoom;
-
+    public static CameraController Instance { get; private set; }
     void Awake()
     {
-        if (Instance == null) Instance = this;
-        else Destroy(gameObject);
-    }
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
 
-    private void Start()
-    {
-        RoomManager.Instance.OnRoomChanged += HandleRoomChanged;
-    }
-
-    public void HandleRoomChanged(Room room)
-    {
-        targetRoom = room;
+        if (player == null) player = GameObject.FindGameObjectWithTag("Player").transform;
+        if (cam == null) cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
     }
 
     void Update()
     {
-        if (targetRoom != null)
-        {
-            BoxCollider roomCollider = targetRoom.GetComponent<BoxCollider>();
-            Collider playerCollider = player.GetComponent<Collider>();
+        var playerPos = player.transform.position;
+        var camPos = cam.transform.position;
 
-            if (roomCollider.bounds.Contains(playerCollider.bounds.min) &&
-                roomCollider.bounds.Contains(playerCollider.bounds.max))
-            {
-                float roomPosX = targetRoom.transform.position.x;
-                targetPos = new Vector3(roomPosX, transform.position.y, transform.position.z);
-                isMoving = true;
-                targetRoom = null;
-            }
-        }
-
-        if (isMoving)
-        {
-            transform.position = Vector3.SmoothDamp(transform.position, targetPos, ref velocity, smoothTime);
-
-            if (Vector3.Distance(transform.position, targetPos) < 0.01f)
-            {
-                transform.position = targetPos;
-                isMoving = false;
-            }
-        }
-    }
-
-    {
+        cam.transform.position = new(playerPos.x, camPos.y, camPos.z);
     }
 }
