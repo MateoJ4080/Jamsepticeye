6df034ec387b399af9f2696045a886201c8c427e
Mate93934 <mateodjm@gmail.com>
2026-06-06 23:15:48 -0300

    feat: add hit flash effect to enemies

--- Assets/Scripts/Health/Health.cs
+++ Assets/Scripts/Health/Health.cs
@@ -1,28 +1,36 @@
 using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+[RequireComponent(typeof(Renderer))]
 public class Health : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 3;
     private int currentHealth;
-
     public int CurrentHealth => currentHealth;
-
     public event Action OnHealthChanged;
+
     [SerializeField] private AudioClip takeDamageSFX;
+
+    [Header("Hit Flash")]
+    [SerializeField] private Color hitColor = Color.white;
+    [SerializeField] private float hitDuration = 0.1f;
+    private Renderer meshRenderer;
+    private Color originalColor;
 
     private void Awake()
     {
         currentHealth = maxHealth;
+        meshRenderer = GetComponent<Renderer>();
     }
 
     public void TakeDamage(int amount)
     {
-        currentHealth = Mathf.Clamp(currentHealth - amount, 0, maxHealth);
         AudioManager.Instance.PlaySFX(takeDamageSFX);
+        StartCoroutine(HitFlash());
+        currentHealth = Mathf.Clamp(currentHealth - amount, 0, maxHealth);
         if (currentHealth <= 0) Die();
-
         OnHealthChanged?.Invoke();
         Debug.Log($"{gameObject.name} damaged, HP: {currentHealth}");
     }
@@ -34,7 +42,6 @@ public class Health : MonoBehaviour
             if (enemy.dropsKey)
             {
                 Collider enemyCollider = GetComponent<Collider>();
-                Collider keyCollider = enemy.keyPrefab.GetComponent<Collider>();
                 float dropPosY = enemyCollider.bounds.center.y;
 
                 Vector3 spawnPos = new(transform.position.x, dropPosY, transform.position.z);
@@ -50,5 +57,15 @@ public class Health : MonoBehaviour
             StartCoroutine(AudioManager.Instance.PlaySFXAndWait(AudioManager.Instance.SFX_PlayerDeath));
             SceneManager.LoadScene("Level");
         }
+    }
+
+    IEnumerator HitFlash()
+    {
+        if (meshRenderer == null) yield break;
+        originalColor = meshRenderer.material.color;
+
+        meshRenderer.material.color = hitColor;
+        yield return new WaitForSeconds(hitDuration);
+        meshRenderer.material.color = originalColor;
     }
 }
